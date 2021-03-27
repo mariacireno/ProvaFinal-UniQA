d95f2014c50ee15c1d980ebee0bf3e41047540bf
Maria Cireno <dev22@redepos.com.br>
2021-03-27 16:10:04 -0300

    cenario do sobreAccenture

--- src/test/java/io/cucumber/uniqa/SobreAccentureSteps.java
+++ src/test/java/io/cucumber/uniqa/SobreAccentureSteps.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
@@ -17,21 +19,22 @@ public class SobreAccentureSteps extends BaseTest {
     // }    
 
 	@Dado("clico no menu sobre a accenture")
-	public void clico_no_menu_sobre_a_accenture() {       
-	    WebElement input = browser.findElement(By.cssSelector("#primaryLink5_SobreaAccenture"));
+	public void clico_no_menu_sobre_a_accenture() {  
+		fecharCookies();     
+		WebElement input = browser.findElement(By.cssSelector("#navigation-menu > div:nth-child(5) > div.gh-item.nav-submenu-label > span"));
 	    input.click();
 	}
 
 	@Dado("clico no item do menu sobre a accenture")
-	public void clico_no_item_do_menu_sobre_a_accenture() {
-	    WebElement input = browser.findElement(By.cssSelector(""));
-	    input.click();
+	public void clico_no_item_do_menu_sobre_a_accenture() throws InterruptedException {
+		Thread.sleep(2000);	    
+		browser.findElement( By.cssSelector("a[href='/br-pt/about/company-index']")).click();
 	}
 
 	@Entao("devo ver o destaque em {string}")
 	public void devo_ver_o_destaque_em(String string) {
-	   WebElement input = browser.findElement(By.cssSelector(".sectionTitle"));
-	   assertEquals(string, input.getText());
+	   WebElement input = browser.findElement(By.cssSelector(".row.new-row"));
+	   assertEquals(true, input.getText().contains(string));
 	   finalizar();
 	}
     
